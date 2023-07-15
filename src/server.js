ee5eca78bdd2437d3bcfdcaa375a1d82380a9478
RyanNealon2 <112398156+RyanNealon2@users.noreply.github.com>
2023-07-14 20:00:50 -0400

    backend

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ try{
 }
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://ai-create-art-2cb1b1626f9c.herokuapp.com/",
     methods: ["GET", "POST"]
   },
 });
@@ -72,6 +72,7 @@ io.on("connection", (socket) => {
     socket.emit("sendRegeneratedUrl", regenerated_url)
   })
 });
-server.listen(5000, () => {
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => {
   console.log("Server is Running")
 })
